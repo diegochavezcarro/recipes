b53a26a5d3a5f75c64358bbe1fbc8ab6e9b7532f
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-16 15:59:04 -0300

    primera parte form reactivo

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import { NavParams } from 'ionic-angular';
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'page-edit-recipe',
@@ -8,8 +9,16 @@ import { Component } from '@angular/core';
 export class EditRecipePage {
   mode='New';
   selectOptions = ['Easy','Medium','Hard'];
+  recipeForm : FormGroup;
   constructor(private navParams:NavParams){}
   ngOnInit(){
     this.navParams.get('mode');
+  }
+  private initializeForm(){
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
   }
 }
