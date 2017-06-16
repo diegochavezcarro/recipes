39661f0c5db0b80f14d7a1621c3ef83f4490f801
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-16 12:41:41 -0300

    corregi mi error sobre los tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,24 +1,13 @@
+import { RecipesPage } from './../recipes/recipes';
+import { ShoppingListPage } from './../shopping-list/shopping-list';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TabsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabsPage');
-  }
-
+  slPage= ShoppingListPage;
+  recipesPage = RecipesPage;
+ 
 }
