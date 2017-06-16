23174b1b8f495cb72c3761a2a1355eb66d9209eb
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-16 13:11:24 -0300

    ngForm

--- src/pages/shopping-list/shopping-list.ts
+++ src/pages/shopping-list/shopping-list.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'page-shopping-list',
   templateUrl: 'shopping-list.html',
 })
 export class ShoppingListPage {
+  onAddItem(form: NgForm) { 
+    console.log(form);
+    
+  }
 
 }
