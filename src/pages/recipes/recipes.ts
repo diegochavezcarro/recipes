578cc70797018b4e6f17e55276c47294689dedb0
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-21 12:52:40 -0300

    section 6 lecture 131

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,3 +1,5 @@
+import { RecipesService } from './../../services/recipes';
+import { Recipe } from './../../models/recipe';
 import { EditRecipePage } from './../edit-recipe/edit-recipe';
 import { NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
@@ -7,8 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
-  constructor(private navCtrl:NavController){}
+  recipes: Recipe[]=[];
+  constructor(private navCtrl:NavController,
+      private recipesService:RecipesService){}
+  ionViewWillEnter(){
+    this.recipes = this.recipesService.getRecipes();
+  }
   newRecipe(){
     this.navCtrl.push(EditRecipePage, {mode:'New'});
+  }
+  onLoadRecipe(){
+
   }
 }
