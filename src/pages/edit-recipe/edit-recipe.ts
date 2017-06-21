52f1fee76e15276d159b21d57ff10cf126170fa1
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-21 14:10:11 -0300

    section 6 lecture 136

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,7 +1,10 @@
+import { Recipe } from './../../models/recipe';
 import { Ingredient } from './../../models/ingredient';
 import { RecipesService } from './../../services/recipes';
-import { NavParams, ActionSheetController, 
-    AlertController,ToastController, NavController } from 'ionic-angular';
+import {
+  NavParams, ActionSheetController,
+  AlertController, ToastController, NavController
+} from 'ionic-angular';
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 
@@ -13,22 +16,42 @@ export class EditRecipePage {
   mode = 'New';
   selectOptions = ['Easy', 'Medium', 'Hard'];
   recipeForm: FormGroup;
+  recipe: Recipe;
+  index: number;
   constructor(private navParams: NavParams,
     private actionSheetController: ActionSheetController,
     private alertCtrl: AlertController,
     private toastCtrl: ToastController,
-    private recipesService:RecipesService,
+    private recipesService: RecipesService,
     private navCtrl: NavController) { }
   ngOnInit() {
-    this.navParams.get('mode');
+    this.mode = this.navParams.get('mode');
+    if (this.mode == 'Edit') {
+      this.recipe = this.navParams.get('recipe');
+      this.index = this.navParams.get('index');
+    }
     this.initializeForm();
   }
   private initializeForm() {
+    let title = null;
+    let description = null;
+    let difficulty = 'Medium';
+    let ingredients = [];
+    if (this.mode == 'Edit'){
+      title = this.recipe.title;
+      description = this.recipe.description;
+      difficulty = this.recipe.difficulty;
+      for (let ingredient of this.recipe.ingredients) {
+        ingredients.push(new FormControl(ingredient.name, Validators.required));
+        
+      }
+    }
+
     this.recipeForm = new FormGroup({
-      'title': new FormControl(null, Validators.required),
-      'description': new FormControl(null, Validators.required),
-      'difficulty': new FormControl('Medium', Validators.required),
-      'ingredients': new FormArray([])
+      'title': new FormControl(title, Validators.required),
+      'description': new FormControl(description, Validators.required),
+      'difficulty': new FormControl(difficulty, Validators.required),
+      'ingredients': new FormArray(ingredients)
     });
   }
   onSubmit() {
@@ -37,13 +60,13 @@ export class EditRecipePage {
     let ingredients = [];
     //los ingredients del form son un array de strings, los q necesita
     //el servicio con arrays de strings y numbers
-    if(value.ingredients.length > 0){
-      ingredients = value.ingredients.map(name=>{
-        return {name:name, amount: 1};
+    if (value.ingredients.length > 0) {
+      ingredients = value.ingredients.map(name => {
+        return { name: name, amount: 1 };
       });
     }
-    this.recipesService.addRecipe(value.title, value.description, 
-        value.difficulty, ingredients);
+    this.recipesService.addRecipe(value.title, value.description,
+      value.difficulty, ingredients);
     this.recipeForm.reset(); //esta de mas
     this.navCtrl.popToRoot();
   }
@@ -76,12 +99,12 @@ export class EditRecipePage {
             }
             (<FormArray>this.recipeForm.get('ingredients'))
               .push(new FormControl(data.name, Validators.required));
-                const toast = this.toastCtrl.create({
-                message: 'Item Added!',
-                duration: 2000,
-                position: 'bottom'
-              });
-              toast.present();
+            const toast = this.toastCtrl.create({
+              message: 'Item Added!',
+              duration: 2000,
+              position: 'bottom'
+            });
+            toast.present();
           }
 
         }
@@ -105,10 +128,10 @@ export class EditRecipePage {
           handler: () => {
             const fArray: FormArray = <FormArray>this.recipeForm.get('ingredients');
             const len = fArray.length;
-            if(len>0){
-              for (let i = len -1; i >=0; i--) {
+            if (len > 0) {
+              for (let i = len - 1; i >= 0; i--) {
                 fArray.removeAt(i);
-                
+
               }
               const toast = this.toastCtrl.create({
                 message: 'All ingredients were deleted!',
